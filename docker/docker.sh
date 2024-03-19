45887c62add2931f47210e75d72eeb0b09e17028
ansost <anna.stein@hhu.de>
2024-03-19 09:27:51 +0100

    move script

--- docker/docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo systemctl start docker
-docker run -it mmcauliffe/montreal-forced-aligner:latest
-docker cp ../data/fleurtg_pronunciation_dict.tsv mmcauliffe/montreal-forced-aligner:latest:/
